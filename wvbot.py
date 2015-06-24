2da62ce955e1849854c9620821ce113343b6589c
William Mathewson <neanias@users.noreply.github.com>
2015-06-24 16:32:14 +0100

    Fix missing optional "'"

--- wvbot.py
+++ wvbot.py
@@ -22,7 +22,7 @@ volunteering_regexes = [
 
 listing_regexes = [
     r'^ *what have i (volunteered for|agreed to|let myself in for|got myself in ?to)',
-    r'^ *(oh,? )?for (fucks|pete\'?s|christ\'?s) sake',
+    r'^ *(oh,? )?for (fuck\'?s|pete\'?s|christ\'?s) sake',
     r'^ *omgwtflol'
 ]
 
